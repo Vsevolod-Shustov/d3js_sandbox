680b37d1a0343d6ac655f4ca516b2570e0773137
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2015-09-03 17:30:58 +0300

    arcs, overalpping

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,3 +4,12 @@ var d3sbControllers = angular.module('d3sbControllers', []);
 d3sbControllers.controller('globalCtrl', ['$scope', 'LocalStorageService', '$location', function($scope, LocalStorageService, $location){
 
 }]);
+
+d3sbControllers.controller('arcCtrl', ['$scope', 'LocalStorageService', '$location', function($scope, LocalStorageService, $location){
+  $scope.values = {
+    'main': 70,
+    'min': 30,
+    'expected': 40
+  };
+  console.log($scope.values);
+}]);
