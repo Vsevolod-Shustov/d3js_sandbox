e54f81086ee2e8c5c53b950b8f8a152e36a98135
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2015-09-04 13:28:07 +0300

    arcs, regular

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,25 +1,26 @@
 "use strict";
 var d3sbDirectives = angular.module('d3sbDirectives', []);
 
-d3sbDirectives.directive('arcDirective', ['$compile', function($compile){
+//overlapping
+d3sbDirectives.directive('arcOverlapping', ['$compile', function($compile){
   return {
     restrict: "A",
     link: function(scope, elem, attrs){
-      var width = 960,
-          height = 500,
+      var width = 450,
+          height = 450,
           radius = height / 2 - 10,
           pi = Math.PI,
           rotateAngle = 1.1*180;
 
       var myScale = d3.scale.linear().domain([0, 100]).range([0, 1.8 * Math.PI]);
       
-      var svg = d3.select("#content").append("svg")
+      var svg = d3.select("#arc_overlapping").append("svg")
           .attr("width", width)
           .attr("height", height)
         .append("g")
           .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
         
-        scope.$watch('values',function(){
+      scope.$watch('values',function(){
         //main
         svg.select("g #pathMain").remove();
         svg.select("g #pathMin").remove();
@@ -66,6 +67,51 @@ d3sbDirectives.directive('arcDirective', ['$compile', function($compile){
           .style("fill", "#e74c3c")
           .attr("d", arcExpected)
           .attr("transform", "rotate("+rotateAngle+" 0 0)");
+        }, true);
+    }
+  }
+}]);
+
+//regular
+d3sbDirectives.directive('arcRegular', ['$compile', function($compile){
+  return {
+    restrict: "A",
+    link: function(scope, elem, attrs){
+      var width = 450,
+          height = 450,
+          radius = height / 2 - 10,
+          pi = Math.PI,
+          rotateAngle = 1.1*180;
+
+      var myScale = d3.scale.linear().domain([0, 100]).range([0, 1.8 * Math.PI]);
+      
+      var svg = d3.select("#arc_regular").append("svg")
+          .attr("width", width)
+          .attr("height", height)
+        .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        
+      scope.$watch('values',function(newData){
+        var data = Object.keys(newData).map(function (key) {return parseInt(newData[key])});
+        console.log(data);
+        svg.selectAll("path").remove();
+        
+        var arc = d3.svg.arc()
+          .innerRadius(radius - 40)
+          .outerRadius(radius)
+          .cornerRadius(20);
+
+        var pie = d3.layout.pie()
+          .padAngle(.02)
+          .sort(null);
+
+        var color = ["#f39c12", "#3498db", "#e74c3c"];
+        
+        svg.selectAll("path")
+          .data(pie(data))
+        .enter().append("path")
+          .style("fill", function(d, i) { return color[i]; })
+          .attr("d", arc);
         }, true);
     }
   }
