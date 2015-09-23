81358e41b82c1ecc6667ac3ee9a62ca357258f7d
Vsevolod <Vsevolod-Shustov@users.noreply.github.com>
2015-09-23 16:20:58 +0300

    basic tree

--- app/js/directives.js
+++ app/js/directives.js
@@ -181,7 +181,7 @@ d3sbDirectives.directive('geoOrthographic', [function(){
       
       var path = d3.geo.path()
         .projection(projection);
-            
+      
       var svg = d3.select(elem[0])
         .append("svg")
         .attr({width:width, height:height})
@@ -207,7 +207,7 @@ d3sbDirectives.directive('geoOrthographic', [function(){
           .on('mouseout', function(d){
             d3.select(this).style("fill", "#222");
             tip.hide(d);
-          })
+          });
         });
       
       var rotation = [0, 0, 0];
@@ -220,6 +220,102 @@ d3sbDirectives.directive('geoOrthographic', [function(){
         projection.rotate(rotation);
         svg.selectAll("path").attr("d", path);
       };
+    }
+  }
+}]);
+
+//tree
+d3sbDirectives.directive('tree', [function(){
+  return {
+    restrict: "A",
+    link: function(scope, elem, attrs){
+      var treeData = [
+        {
+          "name": "Top Level",
+          "parent": "null",
+          "children": [
+            {
+              "name": "Level 2: A",
+              "parent": "Top Level",
+              "children": [
+                {
+                  "name": "Son of A",
+                  "parent": "Level 2: A"
+                },
+                {
+                  "name": "Daughter of A",
+                  "parent": "Level 2: A"
+                }
+              ]
+            },
+            {
+              "name": "Level 2: B",
+              "parent": "Top Level"
+            }
+          ]
+        }
+      ];
+      var margin = {top: 40, right: 120, bottom: 20, left: 120},
+        width = elem[0].offsetWidth - margin.right - margin.left,
+        height = width/2 - margin.top - margin.bottom;
+      var i = 0;
+      var tree = d3.layout.tree()
+        .size([height, width]);
+
+      var diagonal = d3.svg.diagonal()
+        .projection(function(d) { return [d.x, d.y]; });
+
+      var svg = d3.select(elem[0]).append("svg")
+        .attr("width", width + margin.right + margin.left)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var root = treeData[0];
+        
+      update(root);
+      
+      function update(source) {
+
+        // Compute the new tree layout.
+        var nodes = tree.nodes(root).reverse(),
+          links = tree.links(nodes);
+
+        // Normalize for fixed-depth.
+        nodes.forEach(function(d) { d.y = d.depth * 100; });
+
+        // Declare the nodes
+        var node = svg.selectAll("g.node")
+          .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+        // Enter the nodes.
+        var nodeEnter = node.enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function(d) { 
+            return "translate(" + d.x + "," + d.y + ")"; });
+
+        nodeEnter.append("circle")
+          .attr("r", 10)
+          .style("fill", "#fff");
+
+        nodeEnter.append("text")
+          .attr("y", function(d) { 
+            return d.children || d._children ? -18 : 18; })
+          .attr("dy", ".35em")
+          .attr("text-anchor", "middle")
+          .text(function(d) { return d.name; })
+          .style("fill-opacity", 1);
+
+        // Declare the linksÅc
+        var link = svg.selectAll("path.link")
+          .data(links, function(d) { return d.target.id; });
+
+        // Enter the links.
+        link.enter().insert("path", "g")
+          .attr("class", "link")
+          .attr("d", diagonal);
+
+      }
     }
   }
 }]);
